62bbe6851c7070f508cd55a76e92d868aa686112
ApVen77 <42076342+ApVen77@users.noreply.github.com>
2023-05-24 21:09:41 -0600

    final touches

---
+++ app.js
@@ -0,0 +1,11 @@
+function answer() {
+  document.getElementById("answer").innerHTML = "NO!";
+}
+
+function answerYes() {
+  document.getElementById("answer1").innerHTML = "YES!";
+}
+
+function answerYes2() {
+  document.getElementById("answer2").innerHTML = "YES!";
+}
